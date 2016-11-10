ba434c2f22c2080b34a77060ac2185d6ff1319cb
Stan Lagun <slagun@mirantis.com>
2016-11-10 15:12:11 -0800

    Remove oslosphinx from sphinx extensions
    Change-Id: I29a3b379761cc85ea4d78d60cebf6a985e8cde0f

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,8 +23,7 @@ sys.path.insert(0, os.path.join(os.path.abspath('.'), '_exts'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
-    # 'sphinx.ext.intersphinx',
-    'oslosphinx',
+    # 'oslosphinx',
     'yaqlautodoc'
 ]
 
